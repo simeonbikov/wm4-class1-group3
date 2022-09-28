c7778c51cd6fcfb57f6a0995a7bac6ce7ee30bdc
simeonbikov <bikovsem@gmail.com>
2022-09-28 01:02:59 +0100

    merged html css js

--- letters.js
+++ letters.js
@@ -1,220 +1,175 @@
-//create canvas:
+// const canvas = document.getElementById("gameCanvas");
 const canvas = document.createElement("canvas");
 canvas.setAttribute("height", "480");
 canvas.setAttribute("width", "640");
-canvas.style.backgroundColor = "yellow";
+canvas.style.position = "absolute";
+canvas.style.top = "300px";
+// canvas.style.top = window.innerHeight / 2 - canvas.height / 2 + "px";
+canvas.style.left = window.innerWidth / 2 - canvas.width / 2 + "px";
+canvas.style.border = "1px solid #000";
+canvas.style.backgroundColor = "#ffd500";
+// canvas.style.backgroundColor = "#ffd500";
 document.body.prepend(canvas);
 const ctx = canvas.getContext("2d");
-const game = {requestAnim: ""};
-const letterMade = {numberOfGeneratedLetters: 50, speed: 1, lettersMade: []};
+
+const lettersMade = [];
 const clickTracker = [];
+const words = ["APPLE", "TOY", "BALL", "BIKE", "RAINBOW"];
+let word = "";
+const game = { requestAnim: "", score: 0 };
 
-canvas.addEventListener('click', (e) => {
-    const rectangle = canvas.getBoundingClientRect();
-    const mouseClick = {
-      x: e.clientX - rectangle.left,
-      y: e.clientY - rectangle.top,
-      size: 15,
-    };
-    clickTracker.push(mouseClick);
-      
-    // letterMade.lettersMade.forEach((letter, index) => {
-    //   if (collisionCheck(letter, mouseClick)) {
-    //     letterMade.lettersMade.splice(index, 1);
-    //   }
-    // });
-
-    // 
-
-    //console.log(mouseClick);
-    // console.log(e.clientX);
-    // console.log(e.clientY);
-    // console.log(rectangle.top);
-    // console.log(rectangle.left);
+canvas.addEventListener("click", (e) => {
+  const rectangle = canvas.getBoundingClientRect();
+  const mouseClick = {
+    x: e.clientX - rectangle.left,
+    y: e.clientY - rectangle.top,
+    size: 10,
+  };
+  clickTracker.push(mouseClick);
 });
 
-function collisionCheck(lett, mouseDot) {
-    let isHit = lett.x < mouseDot.x + mouseDot.size && lett.x + lett.size > mouseDot.x && lett.y < mouseDot.y + mouseDot.size && lett.y + lett.size > mouseDot.y;
-    if (isHit) {
-        console.log(lett);
-        console.log(mouseDot);
-    }
-    return isHit;
+function collisionCheck(lett, dot) {
+  let isHit =
+    lett.x < dot.x + dot.size &&
+    lett.x - lett.size + 10 + lett.size > dot.x &&
+    lett.y - lett.size - 10 < dot.y + dot.size &&
+    lett.y + lett.size > dot.y;
+  return isHit;
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    if (letterMade.lettersMade.length < letterMade.numberOfGeneratedLetters) {
-      // create new letter:
-      letterMaker();
-    }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  letterMaker();
 
-    clickTracker.forEach((dot, index) => {
-        // ctx.fillStyle = "white";
-        ctx.strokeStyle = "orange";
-        ctx.beginPath();
-        // ctx.fillRect(dot.x, dot.y, dot.size, dot.size);
-        ctx.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
-        // ctx.fill();
-        ctx.stroke();
-        dot.size -= 1;
-        if (dot.size < 1) {
-            clickTracker.splice(index, 1);
+  clickTracker.forEach((dot, index) => {
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    dot.size -= 1;
+    if (dot.size < 1) {
+      clickTracker.splice(index, 1);
+    }
+  });
+  lettersMade.forEach((lett, index) => {
+    lett.y += lett.speed;
+    lett.x -= Math.random() * 2 - 1; // moving left/right
+    if (lett.y < -0.5) {
+      lettersMade.splice(index, 1);
+    }
+    clickTracker.forEach((dot) => {
+      if (collisionCheck(lett, dot)) {
+        lettersMade.splice(index, 1);
+        // console.log(lett);
+        if (word.includes(lett.letter)) {
+          word = word.replace(lett.letter, "");
+          game.score--;
+          if (game.score === 0) {
+            alert("Congratulations! You got the word!");
+            document.location.reload();
+            clearInterval(interval); // for Chrome to end game
+          }
         }
-    });
-    
-    letterMade.lettersMade.forEach((lett, index) => {
-      lett.y -= letterMade.speed; // speed of letters
-      lett.x -= Math.random() * 0.3 - 0.1; // moving left/right
-      if (lett.y < 0) {
-        let temp = letterMade.lettersMade.splice(index, 1);
-        // console.log(temp);
       }
-      clickTracker.forEach((dot) => {
-        if (collisionCheck(lett, dot)) {
-            letterMade.lettersMade.splice(index, 1);
-        }
-      });
-      drawLetter(lett.x, lett.y, lett.size, lett.letter, lett.color);
     });
-    game.requestAnim = requestAnimationFrame(draw);
+    drawLetter(lett.x, lett.y, lett.size, lett.letter, lett.color);
+  });
+  //score area:
+  game.score = word.length;
+  ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+  ctx.fillRect(canvas.width / 2 - 120, 13, 240, 40);
+  ctx.beginPath();
+  ctx.fillStyle = "#ffd500";
+  ctx.font = "24px serif";
+  ctx.textAlign = "center";
+  let startBtn = `LETTERS LEFT: ${game.score}`;
+  ctx.fillText(startBtn, canvas.width / 2, 40);
+
+  game.requestAnim = requestAnimationFrame(draw);
 }
 
-
 function letterMaker() {
-    const letters = ["A", "B", "C", "D", "E"];
-    const colors = ["red", "green", "blue", "black"];
+  const lettersList = words.join("");
+  const colorsLetterList = ["#e01e37", "#3f37c9", "#7209b7", "#008000"];
+  let randomLetter =
+    lettersList[Math.floor(Math.random() * lettersList.length)];
+  let randomColor =
+    colorsLetterList[Math.floor(Math.random() * colorsLetterList.length)];
+  let letterSize = Math.random() * 60 + 40;
+  let letterSpeed = Math.random() * 1.5;
+  let xPosition = Math.random() * (canvas.width - letterSize);
+  let yPosition = Math.random() * (0 - letterSize);
 
-    const getRandomLetter = (lettersArr) =>
-    lettersArr[Math.floor(Math.random() * lettersArr.length)];
-
-    const getRandomColor = (colorsArr) =>
-    colorsArr[Math.floor(Math.random() * colorsArr.length)];
-
-    const randomLetter = getRandomColor(colors);
-    const randomColor = getRandomLetter(letters);
-
-    let letterSize = Math.random() * 60 + 15;
-    let xPosition = Math.random() * (canvas.width - letterSize);
-    let yPosition = Math.random() * (canvas.height - letterSize) + canvas.height;
-
-    letterMade.lettersMade.push({
-        x: xPosition,
-        y: yPosition,
-        size: letterSize,
-        letter: randomLetter,
-        color: randomColor,
-    });
+  lettersMade.push({
+    x: xPosition,
+    y: yPosition,
+    size: letterSize,
+    speed: letterSpeed,
+    letter: randomLetter,
+    color: randomColor,
+  });
 }
 
 function drawLetter(xPosition, yPosition, letterSize, randomLetter, randomColor) {
-    ctx.font = letterSize + "px serif";
-    ctx.fillStyle = randomLetter;
-    ctx.fillText(randomColor, xPosition, yPosition);
+  ctx.font = "bold " + letterSize + "px serif";
+  ctx.fillStyle = randomColor;
+  ctx.fillText(randomLetter, xPosition, yPosition);
 }
-
-game.requestAnim = requestAnimationFrame(draw);
-
-
-
-
-
-
-
-
-
-
-
-// const ctx = canvas.getContext("2d");
-
-// const gradient = ctx.createRadialGradient(canvas.width/2,canvas.height/2-20, 20,canvas.width/2,canvas.height/2,50);
-// gradient.addColorStop(0, "rgba(0,0,255,0.9)");
-// gradient.addColorStop(1, "rgba(255,255,255,0.1)");
-
-// ctx.beginPath();
-// ctx.fillStyle = gradient;
-// ctx.strokeStyle = "rgba(255,255,255,0.4)";
-// ctx.arc(canvas.width/2,canvas.height/2,50,0,Math.PI * 2);
-// ctx.fill();      // fill the color whole circle
-// ctx.stroke();    // bubble border
-
-
-
-//----------------
-
-// //create canvas:
-// const canvas = document.createElement("canvas");
-// canvas.setAttribute("height", "480");
-// canvas.setAttribute("width", "640");
-// canvas.style.backgroundColor = "yellow";
-// document.body.prepend(canvas);
-// const ctx = canvas.getContext("2d");
-// const game = {requestAnim: ""};
-// const letterMade = {numberOfGeneratedLetters: 50, lettersMade: []};
-
-// // for (let i = 0; i < letterMade.numberOfGeneratedLetters; i++) {
-// //   createLetter();
-// // }
-
-// function draw() {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-//     if (letterMade.lettersMade.length < letterMade.numberOfGeneratedLetters) {
-//       // create new letter:
-//       letterMaker();
-//     }
-//     letterMade.lettersMade.forEach((letter, index) => {
-//         letter.y--;
-//         drawLetter(letter.x, letter.y, letter.size, letter.letter, letter.color);
-//     });
-//     game.requestAnim = requestAnimationFrame(draw);
-// }
-
-// function letterMaker() {
-//     const letters = ["A", "B", "C", "D", "E"];
-//     const colors = ["red", "green", "blue", "black"];
-
-//     const getRandomLetter = (lettersArr) =>
-//     lettersArr[Math.floor(Math.random() * lettersArr.length)];
-
-//     const getRandomColor = (colorsArr) =>
-//     colorsArr[Math.floor(Math.random() * colorsArr.length)];
-
-//     const randomLetter = getRandomColor(colors);
-//     const randomColor = getRandomLetter(letters);
-
-//   let letterSize = Math.random() * 60 + 15;
-//   let xPosition = Math.random() * (canvas.width - letterSize);
-//   let yPosition = Math.random() * (canvas.height - letterSize);
-
-//     letterMade.lettersMade.push({
-//         x: xPosition,
-//         y: yPosition,
-//         size: letterSize,
-//         letter: randomLetter,
-//         color: randomColor,
-//     });
-// }
-
-// function drawLetter(xPosition, yPosition, letterSize, randomLetter, randomColor) {
-// // get random letter
-// //   const letters = ["A", "B", "C", "D", "E"];
-// //   const colors = ["red", "green", "blue", "black"];
-// //   let letterSize = Math.random() * 60 + 15;
-// //   let xPosition = Math.random() * (canvas.width - letterSize);
-// //   let yPosition = Math.random() * (canvas.height - letterSize);
-
-// //   const getRandomLetter = (lettersArr) =>
-// //   lettersArr[Math.floor(Math.random() * lettersArr.length)];
-
-// //   const getRandomColor = (colorsArr) =>
-// //   colorsArr[Math.floor(Math.random() * colorsArr.length)];
-
-//   ctx.font = letterSize + "px serif";
-// //   ctx.strokeText("Hello world", 10, 50);
-//   ctx.fillStyle = randomLetter;
-//   ctx.fillText(randomColor, xPosition, yPosition);
-// }
 
 // game.requestAnim = requestAnimationFrame(draw);
 
-//----------------
+function startGame() {
+  // canvas.getAttribute("hidden");
+  // canvas.removeAttribute("hidden");
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
+  ctx.fillRect(canvas.width/2 -120, canvas.height/2 -40, 240, 40);
+  ctx.beginPath();
+  ctx.fillStyle = "#ffd500";
+  ctx.font = "24px serif";
+  ctx.textAlign = "center";
+  let startTxt = "START";
+  ctx.fillText(startTxt, canvas.width/2, canvas.height/2 -12, 400);
+
+  canvas.addEventListener("click", oneClick);
+  function oneClick(e) {
+    this.removeEventListener("click", oneClick);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.requestAnim = requestAnimationFrame(startIntro);
+  }
+}
+
+function startIntro() {
+  word = words[Math.floor(Math.random() * words.length)];
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
+  ctx.fillRect(0, canvas.height / 2 - 40, canvas.width, 40);
+  ctx.beginPath();
+  ctx.fillStyle = "#ffd500";
+  ctx.font = "24px serif";
+  ctx.textAlign = "center";
+  let introTxt = `Remember this word: "${word}"`;
+  ctx.fillText(introTxt, canvas.width / 2, canvas.height / 2 - 12, 400);
+
+  canvas.addEventListener("click", oneClick);
+  // timer();
+  function oneClick(e) {
+    this.removeEventListener("click", oneClick);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.requestAnim = requestAnimationFrame(draw);
+  };
+}
+
+  // function timer() {
+  //   let seconds = 10;
+  //   let id = window.setInterval(function() {
+  //     seconds--;
+  //     if (seconds < 0) {
+  //       clearInterval(id);
+  //       game.requestAnim = requestAnimationFrame(draw);
+  //       return;
+  //     }
+  //   }, 1000 / 60);
+  // };
+
+startGame();
